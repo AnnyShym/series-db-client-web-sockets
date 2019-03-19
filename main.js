ca60e3753634ac07320f48cb7e8d29685281a140
AnnyShym <AnnyShym@gmail.com>
2019-03-19 23:48:41 +0300

    almost ready

--- main.js
+++ main.js
@@ -1,38 +1,102 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'series'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySql connected.');
+});
 
 var app = express();
 
 app.set('view engine', 'ejs');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.post('/index/:id', urlencodedParser, function(req, res) {
-  if (req.params.id != 2) {
-    res.render("update");
-    // save index, operation
-  }
-  else {
-    // db, status
-    res.render("index");
-  }
-});
+function insert(table, newSeries) {
+    let sql = `INSERT INTO ${table} SET ?`;
+    let query = db.query(sql, newSeries, (err, results) => {
+      if (err) {
+          throw err;
+      }
+      return results;
+    });
+}
 
-app.post('/update', urlencodedParser, function(req, res) {
-  // validation
-  // db
-  // return res.sendStatus(400);
-  res.render("index");
-});
+function del(table, id) {
+    let sql = `DELETE FROM ${table} WHERE id = ${id}`;
+    let query = db.query(sql, (err, results) => {
+      if (err) {
+          throw err;
+      }
+      return results;
+    });
+}
+
+function update(table, column, id, newValue) {
+    let sql = `UPDATE ${table} SET ${column} = ${newValue} WHERE id = ${id}`;
+    let query = db.query(sql, (err, results) => {
+      if (err) {
+          throw err;
+      }
+      return results;
+    });
+}
 
 app.get('/', function(req, res) {
-  // selecting, redrawing
-  //res.render('index', {someInfo: "Данные для передачи"});
-  res.render("index");
+  let sql = `SELECT * FROM series`;
+  let query = db.query(sql, (err, results) => {
+    if (err) {
+        throw err;
+    }
+    res.render('index', {data: results});
+  });
 });
 
-app.get('/update', function(req, res) {
-  res.render("index");
+app.post('/delete/:id', urlencodedParser, function(req, res) {
+    del('series', req.params.id)
+    res.render('index');
+}
+
+var operation = null;
+var id = 0;
+app.post('/update/:id', urlencodedParser, function(req, res) {
+  operation = 'Update';
+  id = req.params.id;
+  res.render('change', {operation: operation});
+}
+
+app.post('/insert', urlencodedParser, function(req, res) {
+  operation = 'Insert';
+  id = -1;
+  res.render('change', {operation: operation});
+}
+
+// res.render('change', {operation: (str[0].toUpperCase() + str.slice(1))});
+
+app.post('/change', urlencodedParser, function(req, res) {
+  // validation
+  if (operation == 'Insert') {
+    insert('series', req.body);
+  }
+  else {
+    update('series', 'title', req.body.title, id);
+    update('series', 'country', req.body.country, id);
+    update('series', 'description', req.body.description, id);
+    update('series', 'rating', req.body.rating, id);
+  }
+  // return res.sendStatus(400);
+  res.render('index');
 });
 
-console.log("listening");
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server started on port 3000.');
+});;
